458675d6f39308e7db8cda3f4ffac143b314aa0f
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-11-13 14:18:48 +0100

    Make opencv3 works on armv7

--- opencv/build.sh
+++ opencv/build.sh
@@ -8,21 +8,21 @@ fi
 
 mkdir build
 cd build
-cmake                                                               \
+cmake                                                                \
     -DPYTHON2_EXECUTABLE=$PREFIX/bin/python                          \
     -DPYTHON2_INCLUDE_DIR=$PREFIX/include/python2.7/                 \
     -DPYTHON2_LIBRARY=$PREFIX/lib/$PY_LIB                            \
     -DPYTHON2_PACKAGES_PATH=$PREFIX/lib/python2.7/site-packages/     \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX                                  \
-    -DWITH_CUDA=OFF                                                 \
-    -DWITH_AVFOUNDATION=OFF                                         \
-    -DWITH_FFMPEG=OFF                                               \
-    -DJPEG_INCLUDE_DIR:PATH=$PREFIX/include                         \
-    -DJPEG_LIBRARY:FILEPATH=$PREFIX/lib/libjpeg.so                  \
-    -DPNG_PNG_INCLUDE_DIR:PATH=$PREFIX/include                      \
-    -DPNG_LIBRARY:FILEPATH=$PREFIX/lib/libpng.so                    \
-    -DZLIB_INCLUDE_DIR:PATH=$PREFIX/include                         \
-    -DZLIB_LIBRARY:FILEPATH=$PREFIX/lib/libz.so                     \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX                                   \
+    -DWITH_CUDA=OFF                                                  \
+    -DWITH_AVFOUNDATION=OFF                                          \
+    -DWITH_FFMPEG=OFF                                                \
+    -DJPEG_INCLUDE_DIR:PATH=$PREFIX/include                          \
+    -DJPEG_LIBRARY:FILEPATH=$PREFIX/lib/libjpeg.so                   \
+    -DPNG_PNG_INCLUDE_DIR:PATH=$PREFIX/include                       \
+    -DPNG_LIBRARY:FILEPATH=$PREFIX/lib/libpng.so                     \
+    -DZLIB_INCLUDE_DIR:PATH=$PREFIX/include                          \
+    -DZLIB_LIBRARY:FILEPATH=$PREFIX/lib/libz.so                      \
     ..
 make
 make install
