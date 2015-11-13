f6c0ac28a62b09cc42e2e6fc299c623eeddbde35
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-11-13 14:18:22 +0100

    Make x264 works on armv7

--- x264/build.sh
+++ x264/build.sh
@@ -2,11 +2,13 @@
 
 mkdir -vp ${PREFIX}/bin;
 
-if [[ $ARCH = 64 ]]; then
-    export CFLAGS="-Wall -g -m64 -pipe -O2 -march=x86-64 -fPIC"
-else
-    export CFLAGS="-Wall -g -m32 -pipe -O2 -march=i386 -fPIC"
-fi
+# if [[ $ARCH = 64 ]]; then
+#     export CFLAGS="-Wall -g -m64 -pipe -O2 -march=x86-64 -fPIC"
+# else
+#     export CFLAGS="-Wall -g -m32 -pipe -O2 -march=i386 -fPIC"
+# fi
+    export CFLAGS="-Wall -g -pipe -O2 -fPIC"
+
 export CXXLAGS="${CFLAGS}"
 #export CPPFLAGS="-I${PREFIX}/include"
 #export LDFLAGS="-L${PREFIX}/lib"
