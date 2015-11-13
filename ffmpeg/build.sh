f2abf212b6487c45f72133ccc70f67c274991f40
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-11-13 14:18:10 +0100

    Update ffmpeg.

--- ffmpeg/build.sh
+++ ffmpeg/build.sh
@@ -2,7 +2,7 @@
 
 mkdir -vp ${PREFIX}/bin;
 
-export CFLAGS="-Wall -g -m64 -pipe -O2 -march=x86-64 -fPIC"
+export CFLAGS="-Wall -g -pipe -O2 -fPIC"
 export CXXLAGS="${CFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
